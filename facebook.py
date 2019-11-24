e48927c3d47517db9410908c5a294d06e5ee40f1
hallel21-meet <52972653+hallel21-meet@users.noreply.github.com>
2019-11-24 19:43:29 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -2,7 +2,7 @@ users = []
 posts = []
 comments = []
 
-class User ():
+class User():
 	def __init__(self, name, email, password):
 		self.name = name
 		self.email = email
@@ -16,31 +16,39 @@ class User ():
 	def remove_friend(self, email):
 		self.friend_list.remove(email)
 
-	def post(self, post, date, likes):
-		post1 = Post(comment, date, likes, self.email)
+	def post(self, post, date, likes, title):
+		post1 = Post(post, date, likes, self.email, title)
 		posts.append(post1)
 		print(self.name + " has posted: " + post)
 		post1.the_date(post1.date)
 
+	def add_comment(self,comment, date, likes, title):
+		for x in posts:
+			if title == x.title:
+				pst = x.title
+		comment1 = Comment(comment, date, likes, self, pst)
+		comments.append(comment1)
+		print(self.name + " has commented " + comment + " on the post- " + pst)
+
 	def get_useInfo(self):
+		my_post = []
+		for x in posts:
+			if self.email == x.author:
+				my_post.append(x)
 		print("Name: " + self.name + "\nEmail: " + self.email + "\nPassword: " + self.password)
-		print("Friends: " + str(self.friend_list) + "\nPosts: " + str(posts))
+		print("Friends: " + str(self.friend_list) + "\nPosts: " + str(x.title))
 
 
 
 
 class Post():
-	def __init__ (self, comment, date, likes, author):
+	def __init__ (self, comment, date, likes, author, title):
 		self.comment = comment
 		self.date = date
 		self.likes = likes
 		self.author = author
+		self.title = title
 
-	def add_comment(self,comment, date, likes, author):
-		comment1 = comment(comment, date, likes, self)
-		comments.append(comment)
-		print("your comment is " + self.comment)
-		print("the other comments are: " + str(self.list_comments))
 
 	def remove_comment(self):
 		self.list_comments.remove(comment)
@@ -53,23 +61,29 @@ class Post():
 		print("likes: " + str(likes) + "M")
 
 
-class comment(Post):
-	def __init__ (self):
-		Post.__init__(self)
-		self.c_text = c_text
+class Comment(Post):
+	def __init__ (self, comment, date, likes, author, title):
+		Post.__init__(self, comment, date, likes, author, title)
+		self.comment = comment
 
 
 
 user1 = User("shoba", "shoba2004@meet.mit.sho", "shoba_the_king")
 user2 = User("doba", "doba334@meet.mit.sym", "doba55512")
 
+Iemail = input("what is your email?")
+Ipass = input("what is your password?")
+for x in users:
+	if Iemail == x.email and Ipass == x.password:
+		print(x.name + " you are logged in!")
+
+
 user2.add_friend(user1.email)
 user1.add_friend(user2.email)
-user1.post("hey", "21.10", 32)
-user2.post("you", "3.2", 4)
+user1.post("hey", "21.10", 32, "fir post")
+user2.post("you", "3.2", 4, "se post")
 user1.get_useInfo()
 user2.remove_friend(user1.email)
 
-
-
+user1.add_comment("yryyrry", "3.24", 233, "se post")
 
