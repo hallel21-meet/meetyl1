f0fb6a4ddd36cae9001fbd155116a667036f8d03
hallel21-meet <52972653+hallel21-meet@users.noreply.github.com>
2020-01-25 22:45:50 +0200

    Before submission
    All pictures are in place
    Added javascript and css
    Game is running

--- Zombie/zombies.js
+++ Zombie/zombies.js
@@ -1,7 +1,18 @@
-tooSlow()
 function tooSlow(){
 	setTimeout(
 		function(){
 			alert("You're dead!!! you were too slow!!!");
-		} , 2000);
+			restart()
+		} , 7000);
+}
+
+function restart() {
+	setTimeout(
+		function(){
+			if (confirm("Would you like to play again?")) {
+    			window.location.replace("zombie.html");
+			} else {
+    			window.location.replace("theEnd.html");
+			}
+		} , 1000);
 }
